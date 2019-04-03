9fa1e988d36efe161d7fed4d166350715f9f358a
Boris Dayma <boris.dayma@gmail.com>
2019-04-03 18:19:31 -0500

    feat: add preprocess script for image dataset

---
+++ src/pre_process.py
@@ -0,0 +1,22 @@
+# Pre-process BDD dataset: https://deepdrive.berkeley.edu/
+# Segmentation mask code for "Void" updated from 255 to 19 for continuous sequence
+
+from pathlib import Path
+from tqdm import tqdm
+from fastai.vision import *
+
+# Load label images
+path_data = Path('../data/bdd100k/seg')
+path_lbl = path_data/'labels'
+lbl_names = get_image_files(path_lbl, recurse = True)
+
+# Replace "Void" mask from 255 to 19 (for better data display)
+# TODO: Process should be *much* faster with cv2 but had issues with conversion
+for lbl_name in tqdm(lbl_names):
+    img = PIL.Image.open(lbl_name)
+    pixels = img.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            if pixels[i,j] == 255:
+                pixels[i, j] = 19
+    img.save(lbl_name)
