19e2c8548ea354577c642c4d41ab81f86783a348
Boris Dayma <boris.dayma@gmail.com>
2019-04-05 21:54:06 -0500

    feat(pre_process): limit dependencies

--- src/pre_process.py
+++ src/pre_process.py
@@ -4,12 +4,13 @@
 
 from pathlib import Path
 from tqdm import tqdm
-from fastai.vision import *
+from fastai.vision import get_image_files
+import PIL
 
 # Load label images
 path_data = Path('../data/bdd100k/seg')
-path_lbl = path_data/'labels'
-lbl_names = get_image_files(path_lbl, recurse = True)
+path_lbl = path_data / 'labels'
+lbl_names = get_image_files(path_lbl, recurse=True)
 
 # Replace "Void" mask from 255 to 19 (for better data display)
 # TODO: Process should be *much* faster with cv2 but had issues with conversion
@@ -18,6 +19,6 @@ for lbl_name in tqdm(lbl_names):
     pixels = img.load()
     for i in range(img.size[0]):
         for j in range(img.size[1]):
-            if pixels[i,j] == 255:
+            if pixels[i, j] == 255:
                 pixels[i, j] = 19
     img.save(lbl_name)
